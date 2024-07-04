77e811a9c6cc5f5ba79ac5e16b0ec800e3446de9
jaekyung <61006212+jaekyungshon@users.noreply.github.com>
2024-07-04 22:03:50 +0900

    [Gold III] Title: 나무 재테크, Time: 888 ms, Memory: 216280 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -78,78 +78,63 @@ list[r][c-1]=[2,3,5]
 
 # 16235 : tree retech
 
+from sys import stdin 
 from collections import deque
+scan=lambda:map(int,stdin.readline().split())
 
-n, m, k = map(int, input().split(' '))
-
-# 양분 양 배열
-a = [list(map(int, input().split(' '))) for _ in range(n)]
-# 초기 밭 배열
-graph = [[5] * n for _ in range(n)]
-# 나무들 상태 저장할 배열
-trees = [[deque() for _ in range(n)] for _ in range(n)]
-# 위치별 죽은 나무들 저장할 배열
-dead_trees = [[list() for _ in range(n)] for _ in range(n)]
-
-# 입력받은 초기 나무 위치, 나이 저장
-for _ in range(m):
-    x, y, z = map(int, input().split(' '))
-    trees[x - 1][y - 1].append(z)
-
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-dy = [0, 1, 1, 1, 0, -1, -1, -1]
-
-# 봄, 여름 처리할 함수
-def spring_summer():
+def spring():
+    remove_trees=[]
     for i in range(n):
         for j in range(n):
-            len_ = len(trees[i][j]) # 현재 위치에 있는 나무 총 개수
-            # 현재 위치의 나무들 탐색
-            for k in range(len_):
-                # 나무가 죽는 경우
-                if graph[i][j] < trees[i][j][k]:
-                    # 죽는 나무들은 따로 저장
-                    for _ in range(k, len_):
-                        dead_trees[i][j].append(trees[i][j].pop())
-                    break
-                # 나무가 양분 먹고 성장하는 경우
+            # 현재 deque의 길이만큼 조회하면서, pop연산을 진행.
+            #popCnt=len(Trees[i][j])
+            for _ in range(len(Trees[i][j])):
+                curAge=Trees[i][j].popleft() # 최소 나이 나무
+                # 제거 조건
+                if curAge>A[i][j]:
+                    remove_trees.append((i,j,curAge))
+                # 양분 먹이기 가능
                 else:
-                    graph[i][j] -= trees[i][j][k]
-                    trees[i][j][k] += 1
-    # 죽은 나무들 만큼 양분 저장
-    for i in range(n):
-        for j in range(n):
-            while dead_trees[i][j]:
-                graph[i][j] += dead_trees[i][j].pop() // 2
+                    Trees[i][j].append(curAge+1) # 나이+1됬기에, 뒤로 이동
+                    A[i][j]-=curAge # 밭 양분정보 갱신
+    return remove_trees
 
-# 가을, 겨울 처리할 함수
+def summer(targets):
+    for r,c,age in targets:
+        A[r][c]+=age//2
+
 def fall_winter():
+    # 가을
     for i in range(n):
         for j in range(n):
-            # 현재 위치의 나무들 탐색
-            for k in range(len(trees[i][j])):
-                # 현재 나무의 나이가 씨를 뿌릴 수 있는 상태인 경우
-                if trees[i][j][k] % 5 == 0:
-                    # 8방향에 씨 뿌림
-                    for t in range(8):
-                        nx = i + dx[t]
-                        ny = j + dy[t]
-                        # 범위 체크
-                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                            continue
-                        # 새로 태어난 나무들 앞으로 삽입
-                        trees[nx][ny].appendleft(1)
-            # 밭에 양분 추가
-            graph[i][j] += a[i][j]
+            for curAge in Trees[i][j]:
+                if curAge%5==0:
+                    for idx in range(8):
+                        nr,nc=i+dx[idx],j+dy[idx]
+                        if 0<=nr<n and 0<=nc<n:
+                            Trees[nr][nc].appendleft(1) # 새로운 나무 추가.
+            # 겨울
+            A[i][j]+=Extra_A[i][j]
 
 
-for i in range(k):
-    spring_summer()
-    fall_winter()
+n,m,k=scan()
+dx=[-1,-1,-1,0,0,1,1,1]
+dy=[-1,0,1,-1,1,-1,0,1]
+A=[[5]*n for _ in [0]*n] # 밭 배열
+Extra_A=[[*scan()] for _ in [0]*n] # 추가되는 양분 배열
+Trees=[[deque([]) for _ in [0]*n] for _ in [0]*n] # 나무 정보들 배열
 
-answer = 0
+for _ in [0]*m:
+    r,c,age=scan()
+    Trees[r-1][c-1].append(age)
+
+for _ in [0]*k:
+    Removes=spring() # 봄 : 제거나무 리턴
+    summer(Removes) # 여름
+    fall_winter() # 가을+겨울
+
+alive_cnt=0
 for i in range(n):
     for j in range(n):
-        answer += len(trees[i][j])
-
-print(answer)
+        alive_cnt+=len(Trees[i][j])
+print(alive_cnt)
