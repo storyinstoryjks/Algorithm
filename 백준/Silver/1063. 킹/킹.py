0e640aee610086ef8ad31d8dc31f1f1abbe03c09
jaekyung <61006212+jaekyungshon@users.noreply.github.com>
2024-07-13 03:06:47 +0900

    [Silver III] Title: 킹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,10 +1,13 @@
-k,s,N=input().split();k=[ord(k[0])-64,int(k[1])];s=[ord(s[0])-64,int(s[1])]
-d={'R':[1,0],'L':[-1,0],'B':[0,-1],'T':[0,1],'RT':[1,1],'LT':[-1,1],'RB':[1,-1],'LB':[-1,-1]}
-for _ in range(int(N)):
- m=d[input()];nx,ny=k[0]+m[0],k[1]+m[1]
- if 0<nx<=8 and 0<ny<=8:
-  if[nx,ny]==s:
-   sx,sy=s[0]+m[0],s[1]+m[1]
-   if 0<sx<=8 and 0<sy<=8:k,s=[nx,ny],[sx,sy]
-  else:k=[nx,ny]
-print(f'{chr(k[0]+64)}{k[1]}\n{chr(s[0]+64)}{s[1]}')
+f=lambda x,y:ord(x)-64+int(y)*10
+g=lambda x:(9>x//10>0<x%10<9)^1
+C=lambda x:chr(x%10+64)+str(x//10)
+a,b,n=input().split()
+k,s=f(*a),f(*b)
+t="0"*8+"L0R"+"8"*8+"T"
+for _ in[0]*int(n):
+ r=0
+ for c in input():r+=t.find(c)-9
+ q=k;k+=r;s+=r*(k==s)
+ if g(s):s,k=k,q
+ if g(k):k=q
+print(C(k),C(s))
