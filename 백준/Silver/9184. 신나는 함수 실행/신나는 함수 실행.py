935cc30c502bf0bb7d2b2d92b6de2b1b83c3de07
JKS <61006212+storyinstoryjks@users.noreply.github.com>
2025-01-08 11:35:37 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 4416 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,34 @@
+# https://www.acmicpc.net/problem/9184
+# 신나는 함수 실행
+# 재귀,DP
+
+input=__import__('sys').stdin.readline
+import sys
+sys.setrecursionlimit(10**8)
+
+def w(a,b,c):
+    if a<=0 or b<=0 or c<=0:
+        return 1
+    if a>20 or b>20 or c>20:
+        if (a,b,c) in dp.keys():
+            return dp[(a,b,c)]
+        return w(20,20,20)
+    if a<b and b<c:
+        if (a,b,c) in dp.keys():
+            return dp[(a,b,c)]
+        dp[(a,b,c)]=w(a,b,c-1)+w(a,b-1,c-1)-w(a,b-1,c)
+        return dp[(a,b,c)]
+    
+    if (a,b,c) in dp.keys():
+        return dp[(a,b,c)]
+    dp[(a,b,c)]=w(a-1,b,c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1)
+    return dp[(a,b,c)]
+    
+
+while True:
+    a,b,c=map(int,input().split())
+    if (a,b,c)==(-1,-1,-1):
+        break
+    dp={}
+    dp[(a,b,c)]=w(a,b,c)
+    print(f"w({a}, {b}, {c}) = {dp[(a,b,c)]}")
