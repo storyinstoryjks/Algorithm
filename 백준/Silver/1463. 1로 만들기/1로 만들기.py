69a9b9ed8a98ec234ea928f86934eefb2d7b6efb
JKS <61006212+storyinstoryjks@users.noreply.github.com>
2025-01-11 11:36:12 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,18 @@
-# https://www.acmicpc.net/problem/1463
-# 1로 만들기
-# 재귀,트리,DP
-
-# Bottom-Up
-
+# Top-Down
 input=__import__('sys').stdin.readline
+
 x=int(input())
-dp=[0]*(x+1)
-
-for i in range(2,x+1):
-    dp[i]=dp[i-1]+1
-    if i%3==0:
-        dp[i]=min(dp[i],dp[i//3]+1)
-    if i%2==0:
-        dp[i]=min(dp[i],dp[i//2]+1)
-
-print(dp[x])
+dp={1:0}
+def rec(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n%3==0) and (n%2==0):
+        dp[n]=min(rec(n//3)+1, rec(n//2)+1)
+    elif n%3==0:
+        dp[n]=min(rec(n//3)+1, rec(n-1)+1)
+    elif n%2==0:
+        dp[n]=min(rec(n//2)+1, rec(n-1)+1)
+    else:
+        dp[n]=rec(n-1)+1
+    return dp[n]
+print(rec(x))
