7b0ce27b19de540b4da77e0a2d75f957af38a148
JKS <61006212+storyinstoryjks@users.noreply.github.com>
2025-01-21 12:36:41 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,25 +1,18 @@
-### TOP-DOWN
+### Bottom-UP
 input=__import__('sys').stdin.readline
 
-def dfs(x):
-    if x==0:
-        dp[0]=[1,0]
-        return dp[x]
-    if x==1:
-        dp[1]=[0,1]
-        return dp[x]
-    
-    if dp[x]!=[0,0]:
-        return dp[x]
-    
-    left=dfs(x-1)
-    right=dfs(x-2)
-    dp[x][0]+=left[0]+right[0]
-    dp[x][1]+=left[1]+right[1]
-    return dp[x]
-    
 for _ in range(int(input())):
     n=int(input())
+    
+    if n in [0,1]:
+        print(*'1001'[n::2])
+        continue
+    
     dp=[[0,0] for _ in range(n+1)]
-    dfs(n)
+    dp[0],dp[1]=[1,0],[0,1]
+    
+    for x in range(2,n+1):
+        dp[x][0]=dp[x-1][0]+dp[x-2][0]
+        dp[x][1]=dp[x-1][1]+dp[x-2][1]
+    
     print(*dp[n])
