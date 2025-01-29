2bb6b1655102ddb9373937ba147bbff529822e0d
JKS <61006212+storyinstoryjks@users.noreply.github.com>
2025-01-29 22:40:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11726
+# 2n 타일링
+# dp
+
+input=__import__('sys').stdin.readline
+
+n=int(input())
+dp=[0]*(1001)
+
+dp[1]=1
+dp[2]=2
+
+for i in range(3,n+1):
+    dp[i]=(dp[i-1]+dp[i-2])%10007
+
+print(dp[n])
